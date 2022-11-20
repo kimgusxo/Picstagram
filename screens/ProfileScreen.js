3be22643af56212e845317c7218e9d5b32420674
Dong-hui-Kim <donghui11777@gmail.com>
2022-11-21 08:25:23 +0900

    [ProfileScreen] 게시물 썸네일 Flatlist UI구현

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,27 +1,89 @@
-import React, {Component} from 'react';
-import {StyleSheet, View, StatusBar, ScrollView} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, StatusBar, FlatList, Text, Dimensions, Image } from 'react-native';
 import ProfileHeader from '../components/ProfileHeader';
 import ProfileInfomation from '../components/ProfileInfomation';
 import FooterMain from '../components/FooterMain';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-function ProfileScreen({navigation, route}) {
+const dataList = [
+  { key: '1', source: require('../assets/images/aimyon.jpg') },
+  { key: '2', source: require('../assets/images/aimyon1.jpg') },
+  { key: '3', source: require('../assets/images/aimyon2.jpg') },
+  { key: '4', source: require('../assets/images/aimyon3.jpg') },
+  { key: '5', source: require('../assets/images/aimyon4.jpg') },
+  { key: '6', source: require('../assets/images/aimyon5.jpg') },
+  { key: '7', source: require('../assets/images/aimyon6.jpg') },
+  { key: '8', source: require('../assets/images/aimyon7.jpg') },
+  { key: '9', source: require('../assets/images/aimyon8.jpg') },
+  { key: '10', source: require('../assets/images/aimyon9.jpg') },
+  { key: '11', source: require('../assets/images/aimyon10.jpg') },
+];
+const numColumns = 3;
+const DEVICE_WIDTH = Dimensions.get('window').width;
+
+function ProfileScreen({ navigation, route }) {
+  // Flatlist formatting
+  const formatData = (dataList, numColumns) => {
+    const totalRows = Math.floor(dataList.length / numColumns);
+    let totalLastRow = dataList.length - totalRows * numColumns;
+
+    while (totalLastRow !== 0 && totalLastRow !== numColumns) {
+      dataList.push({ key: 'blank', empty: true });
+      totalLastRow++;
+    }
+    return dataList;
+  };
+
+  // Flatlint Rendering
+  const _renderItem = ({ item, index }) => {
+    if (item.empty) {
+      return <View style={[styles.item, styles.blankItem]} />;
+    }
+
+    return (
+      <TouchableOpacity style={styles.item}>
+        <Image style={styles.itemImg} source={item.source} resizeMode={'contain'} />
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <>
       <View>
         <StatusBar hidden />
         <ProfileHeader style={styles.profileHeader} navigation={navigation} />
         <ProfileInfomation style={styles.profileInfomation} navigation={navigation} />
-        <View style={styles.profileHeaderColumnFiller} />
       </View>
-      <ScrollView style={styles.container} />
-      <View>
-        <FooterMain style={styles.footerMain} navigation={navigation} />
+      <View style={{ padding: 4, flex: 1 }}>
+        <FlatList
+          data={formatData(dataList, numColumns)}
+          renderItem={_renderItem}
+          keyExtractor={(item, index) => index.toString()}
+          numColumns={numColumns}
+        />
       </View>
+
+      <FooterMain style={styles.footerMain} navigation={navigation} />
     </>
   );
 }
 
 const styles = StyleSheet.create({
+  item: {
+    backgroundColor: '#6666ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: DEVICE_WIDTH / numColumns,
+    height: DEVICE_WIDTH / numColumns,
+    margin: 4,
+    flex: 1,
+  },
+  blankItem: {
+    backgroundColor: 'transparent',
+  },
+  itemImg: {
+    width: DEVICE_WIDTH / numColumns,
+  },
   container: {
     flex: 1,
   },
@@ -39,9 +101,6 @@ const styles = StyleSheet.create({
   profileInfomation: {
     height: 148,
     backgroundColor: 'white',
-  },
-  profileHeaderColumnFiller: {
-    flex: 1,
   },
   footerMain: {
     height: 56,
