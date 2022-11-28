982578b0df0c36b234fc314fc9cf35fd58f34147
kimgusxo <81300338+kimgusxo@users.noreply.github.com>
2022-11-29 07:31:51 +0900

    eslint 적용본

--- api/PostApi.js
+++ api/PostApi.js
@@ -1,79 +1,103 @@
-import firestore from "@react-native-firebase/firestore"
-import { getPostDocId, getCommentsDocId, getImageDocId } from './LogicApi'
-import { findFollowingById } from './UserApi'
-import { getImageUrl, imageUpload, metadataImage, setMetadata, deleteStorageImage } from './StorageImage'
+import firestore from '@react-native-firebase/firestore';
+import { getPostDocId, getCommentsDocId, getImageDocId } from './LogicApi';
+import { findFollowingById } from './UserApi';
+import {
+  getImageUrl,
+  imageUpload,
+  metadataImage,
+  setMetadata,
+  deleteStorageImage,
+} from './StorageImage';
 
 // 매개변수: 유저ID
-async function findPostById(userId) { //유저ID로 게시물 찾기
+async function findPostById(userId) {
+  //유저ID로 게시물 찾기
   const result = [];
 
-  const postList = await firestore().collection('Post')
-  .where('writer', '==', userId).orderBy('date', 'desc').get() // 두 개 이상의 조건 사용 시 콘솔에서 복합색인 만들어야 함
+  const postList = await firestore()
+    .collection('Post')
+    .where('writer', '==', userId)
+    .orderBy('date', 'desc')
+    .get(); // 두 개 이상의 조건 사용 시 콘솔에서 복합색인 만들어야 함
 
-  if(postList.empty) { // 게시물이 없을때 출력문
-    console.log("해당하는 게시물이 없습니다.");
+  if (postList.empty) {
+    // 게시물이 없을때 출력문
+    console.log('해당하는 게시물이 없습니다.');
     return;
   }
-  
-  postList.forEach(doc => { // 콘솔 출력문
-    result.push(doc.data())
+
+  postList.forEach((doc) => {
+    // 콘솔 출력문
+    result.push(doc.data());
   });
-  
-  return result
+
+  return result;
 }
 
 // 매개변수: 게시물 제목, 시작구간
 // 시작 구간을 받으면 거기부터 10개 출력
-async function findPostByTitle({title, start}) { // 게시물 제목으로 게시물 찾기
+async function findPostByTitle({ title, start }) {
+  // 게시물 제목으로 게시물 찾기
   const result = [];
 
   // limit부분을 start와 end로 바꿀 것
-  const postList = await firestore().collection('Post')
-  .where('title', '==', title).orderBy('date', 'desc').start(start).end(start+10).get(); // 게시물 검색 시 사용함으로 10개씩 받아옴
-  
-  if(postList.empty) {
-    console.log("해당하는 게시물이 없습니다.");
+  const postList = await firestore()
+    .collection('Post')
+    .where('title', '==', title)
+    .orderBy('date', 'desc')
+    .start(start)
+    .end(start + 10)
+    .get(); // 게시물 검색 시 사용함으로 10개씩 받아옴
+
+  if (postList.empty) {
+    console.log('해당하는 게시물이 없습니다.');
     return;
   }
-  postList.forEach(doc => { // 콘솔 출력문
-    result.push(doc.data())
+  postList.forEach((doc) => {
+    // 콘솔 출력문
+    result.push(doc.data());
   });
 
   return result;
 }
 
 // 매개변수: 유저ID
-async function findPostList(userId) { // 맨 처음 메인페이지에다가 게시물 출력함
+async function findPostList(userId) {
+  // 맨 처음 메인페이지에다가 게시물 출력함
   const postList = [];
-  
+
   // 0. 나의 게시물을 배열에 푸쉬
-  const myPost = await findPostById(userId)
-  myPost.forEach(doc => {
-    postList.push(doc)
-  })
-    
+  const myPost = await findPostById(userId);
+  myPost.forEach((doc) => {
+    postList.push(doc);
+  });
+
   // 1. 이메일로 following들을 받아옴
-  const following = await findFollowingById(userId)
-  
+  const following = await findFollowingById(userId);
+
   // 2. 팔로잉 리스트들의 아이디를 통해 게시물들을 받아옴
-  for(const index of following) { // forEach는 await을 기다려주지 않아 for ...of문으로 대체했습니다.
-    const post = await findPostById(index.following) // 1차 반복문에서 팔로잉을 받아온 뒤
-    post.forEach(doc => { // 2차 반복문에서 팔로잉의 게시물을 전체 배열에 푸쉬
-      if(doc.data().range != "Private") { // 공개범위가 비공개가 아닐 때
-        postList.push(doc)
+  for (const index of following) {
+    // forEach는 await을 기다려주지 않아 for ...of문으로 대체했습니다.
+    const post = await findPostById(index.following); // 1차 반복문에서 팔로잉을 받아온 뒤
+    post.forEach((doc) => {
+      // 2차 반복문에서 팔로잉의 게시물을 전체 배열에 푸쉬
+      if (doc.data().range != 'Private') {
+        // 공개범위가 비공개가 아닐 때
+        postList.push(doc);
       }
-    })
+    });
   }
-  
+
   // 3. temp배열에 있는 doc들을 전부 시간순으로 재정렬
   postList.sort(function compare(a, b) {
-    return a.date - b.date
+    return a.date - b.date;
   });
- 
-  postList.forEach(doc => { // 콘솔 출력문
-    console.log(doc.data())
-  })
-  
+
+  postList.forEach((doc) => {
+    // 콘솔 출력문
+    console.log(doc.data());
+  });
+
   // 4. 최종리스트를 10개씩 뽑아서 출력 --> 이 부분을 잘 모르겠음,,,
   // 이슈!: 이거는 값을 최대로 가져오게 되는데, 최적화를 위해 포인터를 설정하는 등의 작업이 필요할 듯 합니다.
   // start와 end로 뽑아오는 방식도 고려중,,,
@@ -98,23 +122,23 @@ async function createPost({ title, content, range, writer, images }) {
   // url을 추출해서 넣는게 아니라 스토리지에 박아넣고 url을 가져와서 입히는 건가??
   // 쨋든 그 작업을 상위 컬렉션의 필드가 채워진 후 넣는 식으로 작성함
 
-  await addStorageImages(images) // 이미지를 스토리지에 넣는 함수
-  await addDatabaseImages({date, metadata}) // 이미지를 데이터베이스에 넣는 함수
+  await addStorageImages(images); // 이미지를 스토리지에 넣는 함수
+  await addDatabaseImages({ date, metadata }); // 이미지를 데이터베이스에 넣는 함수
 }
 
 // 게시물 수정 함수
-async function updatePost({postDate, title, content, images}) {
-  const postDocId = getPostDocId(postDate)
+async function updatePost({ postDate, title, content, images }) {
+  const postDocId = getPostDocId(postDate);
 
   await firestore().collection('Post').doc(postDocId).update({
     title: title,
-    content: content
-  })
+    content: content,
+  });
 
-  await deleteImage(postDocId)
+  await deleteImage(postDocId);
 
-  await addStorageImages(images) // 이미지를 스토리지에 넣는 함수
-  await addDatabaseImages({postDate, metadata}) // 이미지를 데이터베이스에 넣는 함수
+  await addStorageImages(images); // 이미지를 스토리지에 넣는 함수
+  await addDatabaseImages({ postDate, metadata }); // 이미지를 데이터베이스에 넣는 함수
 }
 
 // images가 배열이므로 반복문 돌려야함
@@ -130,7 +154,7 @@ async function addStorageImages(images) {
 }
 
 // images가 배열이므로 반복문 돌려야함
-async function addDatabaseImages({postDate, metadata}) {
+async function addDatabaseImages({ postDate, metadata }) {
   const postDocId = getPostDocId(postDate); // 사진을 넣기 위한 DocId 추출
 
   for (const index of metadata) {
@@ -153,13 +177,17 @@ async function addDatabaseImages({postDate, metadata}) {
 }
 
 async function deleteImage(postDocId) {
-  const imageDocIdList = await firestore().collection('Post') // 해당 하위 컬렉션의 docId를 전부 획득
-  .doc(postDocId).collection('Images').get();
+  const imageDocIdList = await firestore()
+    .collection('Post') // 해당 하위 컬렉션의 docId를 전부 획득
+    .doc(postDocId)
+    .collection('Images')
+    .get();
 
-  imageDocIdList.forEach(doc => { // 반복문으로 하위 컬렉션의 문서를 전부 삭제
+  imageDocIdList.forEach((doc) => {
+    // 반복문으로 하위 컬렉션의 문서를 전부 삭제
     const imagesDocId = doc.id;
-    deleteImagesByDocId({postDocId, imagesDocId})
-  })
+    deleteImagesByDocId({ postDocId, imagesDocId });
+  });
 }
 
 // 이슈!: 파이어베이스는 유일성필드를 지원하지 않는다.
@@ -167,25 +195,33 @@ async function deleteImage(postDocId) {
 // 따라서 게시물과 댓글에 Date()를 통한 값을 유일성으로 생각하고 작성하겠습니다.
 
 // 매개변수: 게시물 시간
-async function deletePost(postDate) { // 게시물 삭제
+async function deletePost(postDate) {
+  // 게시물 삭제
   const postDocId = await getPostDocId(postDate); // 해당 상위컬렉션인 게시물의 docId 획득
-  
-  const commentsDocIdList = await firestore().collection('Post') // 해당 하위 컬렉션의 docId를 전부 획득
-  .doc(postDocId).collection('Comments').get();
 
-  const imageDocIdList = await firestore().collection('Post') // 해당 하위 컬렉션의 docId를 전부 획득
-  .doc(postDocId).collection('Images').get();
-  
-  commentsDocIdList.forEach(doc => { // 반복문으로 하위 컬렉션의 문서를 전부 삭제
+  const commentsDocIdList = await firestore()
+    .collection('Post') // 해당 하위 컬렉션의 docId를 전부 획득
+    .doc(postDocId)
+    .collection('Comments')
+    .get();
+
+  const imageDocIdList = await firestore()
+    .collection('Post') // 해당 하위 컬렉션의 docId를 전부 획득
+    .doc(postDocId)
+    .collection('Images')
+    .get();
+
+  commentsDocIdList.forEach((doc) => {
+    // 반복문으로 하위 컬렉션의 문서를 전부 삭제
     const commentsDocId = doc.id;
-    deleteCommentsByDocId({postDocId, commentsDocId})
-  })
-  
+    deleteCommentsByDocId({ postDocId, commentsDocId });
+  });
 
-  imageDocIdList.forEach(doc => { // 반복문으로 하위 컬렉션의 문서를 전부 삭제
+  imageDocIdList.forEach((doc) => {
+    // 반복문으로 하위 컬렉션의 문서를 전부 삭제
     const imagesDocId = doc.id;
-    deleteImagesByDocId({postDocId, imagesDocId})
-  })
+    deleteImagesByDocId({ postDocId, imagesDocId });
+  });
 
   // 이슈!: 하위 컬렉션이 있는 문서의 경우 하위컬렉션의 모든 문서를 제거한 뒤
   // 삭제해야하므로 for문을 통해 하위컬렉션의 문서들을 전부 삭제해야한다.
@@ -193,59 +229,73 @@ async function deletePost(postDate) { // 게시물 삭제
 }
 
 // 매개변수: 게시물 시간, 설정한 범위
-async function postRangeUpdate({postDate, range}) { // 게시물 범위를 업데이트하는 함수
+async function postRangeUpdate({ postDate, range }) {
+  // 게시물 범위를 업데이트하는 함수
   const postDocId = await getPostDocId(postDate);
-  
+
   await firestore().collection('Post').doc(postDocId).update({
-    range: range
-  })
+    range: range,
+  });
 }
 
 // 매개변수: 게시물 시간, 좋아요 토큰, 좋아요 개수
 // 증분함수나 집계함수가 안먹힙니다....
-async function likeUpdate({postDate, likeToken, like}) { // 좋아요 받는 함수
-  const postDocId = await getPostDocId(postDate)
-  
-  if(likeToken == 'a') { // 토큰 부분은 어케할지 합치면서 수정이다요~
-    await firestore().collection('Post').doc(postDocId).update({
-      like: like+1
-    })
+async function likeUpdate({ postDate, likeToken, like }) {
+  // 좋아요 받는 함수
+  const postDocId = await getPostDocId(postDate);
+
+  if (likeToken == 'a') {
+    // 토큰 부분은 어케할지 합치면서 수정이다요~
+    await firestore()
+      .collection('Post')
+      .doc(postDocId)
+      .update({
+        like: like + 1,
+      });
   } else {
-    await firestore().collection('Post').doc(postDocId).update({
-      like: like-1
-    })
+    await firestore()
+      .collection('Post')
+      .doc(postDocId)
+      .update({
+        like: like - 1,
+      });
   }
 }
 
 // 매개변수: 댓글 작성자, 댓글 내용, 게시물 시간
-async function createComments({commentWriter, commentContent, postDate}) { // 댓글 작성자와 댓글 내용을 넣어 댓글 생성
+async function createComments({ commentWriter, commentContent, postDate }) {
+  // 댓글 작성자와 댓글 내용을 넣어 댓글 생성
   const postDocId = await getPostDocId(postDate);
-  
+
   await firestore().collection('Post').doc(postDocId).collection('Comments').add({
     commentWriter: commentWriter,
     commentContent: commentContent,
     date: new Date(),
-    type: true
-  })
+    type: true,
+  });
 }
 
 // 이슈!: 댓글도 유일성을 보장하는 필드가 없음!
 // 매개변수: 게시물 시간, 댓글 시간
-async function deleteComments({postDate, commentsDate}) {
+async function deleteComments({ postDate, commentsDate }) {
   const postDocId = await getPostDocId(postDate); // 해당 상위컬렉션인 게시물의 docId 획득
-  const commentsDocId = await getCommentsDocId({postDate, commentsDate}); // 해당 하위컬렉션인 댓글의 docId 획득
-    
-  deleteCommentsByDocId({postDocId, commentsDocId});
+  const commentsDocId = await getCommentsDocId({ postDate, commentsDate }); // 해당 하위컬렉션인 댓글의 docId 획득
+
+  deleteCommentsByDocId({ postDocId, commentsDocId });
 }
 
-async function updateComments({postDate, commentsDate, commentContent}) {
+async function updateComments({ postDate, commentsDate, commentContent }) {
   const postDocId = await getPostDocId(postDate); // 해당 상위컬렉션인 게시물의 docId 획득
-  const commentsDocId = await getCommentsDocId({postDate, commentsDate}); // 해당 하위컬렉션인 댓글의 docId 획득
+  const commentsDocId = await getCommentsDocId({ postDate, commentsDate }); // 해당 하위컬렉션인 댓글의 docId 획득
 
-  await firestore().collection('Post').doc(postDocId)
-  .collection('Comments').doc(commentsDocId).set({
-    commentContent: commentContent
-  })
+  await firestore()
+    .collection('Post')
+    .doc(postDocId)
+    .collection('Comments')
+    .doc(commentsDocId)
+    .set({
+      commentContent: commentContent,
+    });
 }
 
 async function readComments(postDate) {
@@ -253,81 +303,111 @@ async function readComments(postDate) {
 
   const result = [];
 
-  const comments = await firestore().collection('Post')
-  .doc(postDocId).collection('Comments').orderBy('date', 'desc').get();
+  const comments = await firestore()
+    .collection('Post')
+    .doc(postDocId)
+    .collection('Comments')
+    .orderBy('date', 'desc')
+    .get();
 
-  comments.forEach(doc => {
-    result.push(doc.data())
-  })
+  comments.forEach((doc) => {
+    result.push(doc.data());
+  });
 
-  return result
+  return result;
 }
 
 // 매개변수: 게시물 시간
-async function readImages(postDate) { // 게시물의 이미지 불러오는 함수
-  const postDocId = getPostDocId(postDate) // 게시물의 DocId 가져옴
+async function readImages(postDate) {
+  // 게시물의 이미지 불러오는 함수
+  const postDocId = getPostDocId(postDate); // 게시물의 DocId 가져옴
 
   const result = [];
 
-  const images = await firestore().collection('Post') // 이미지리스트 가져오기
-  .doc(postDocId).collection('Images').get();
+  const images = await firestore()
+    .collection('Post') // 이미지리스트 가져오기
+    .doc(postDocId)
+    .collection('Images')
+    .get();
 
-  images.forEach(doc => {
-    result.push(doc.data())
-  })
+  images.forEach((doc) => {
+    result.push(doc.data());
+  });
 
-  return result
+  return result;
 }
-
 
 // 어차피 매개변수 두개받아서 프로필 검사하는데 그게 함수로 빼야하나???
 // ---> 잘몰루!
 
 // 매개변수: 리턴된 게시물, 나의Id
-function isMyPost({post, myId}) { // 나의 게시물인지 확인하는 함수
+function isMyPost({ post, myId }) {
+  // 나의 게시물인지 확인하는 함수
   const myPost = [];
-  
-  post.forEach(el => {
-    if(el.writer == myId) {
-      myPost.push(true)
+
+  post.forEach((el) => {
+    if (el.writer == myId) {
+      myPost.push(true);
     } else {
-      myPost.push(false)
+      myPost.push(false);
     }
-  })
-  
+  });
+
   return myPost;
 }
 
 // 매개변수: 리턴된 댓글, 나의Id
-function isMyComments({comments, myId}) { // 내가 쓴 댓글인지 확인하는 함수
+function isMyComments({ comments, myId }) {
+  // 내가 쓴 댓글인지 확인하는 함수
   const myComments = [];
-  
-  comments.forEach(el => {
-    if(el.writer == myId) {
-      myComments.push(true)
+
+  comments.forEach((el) => {
+    if (el.writer == myId) {
+      myComments.push(true);
     } else {
-      myComments.push(false)
+      myComments.push(false);
     }
-  })
-  
+  });
+
   return myComments;
 }
 
 // 게시물의 삭제를 위해 어쩔 수 없이 docId로 삭제하는 방향으로 작성
 // 내부로직 함수
-async function deleteCommentsByDocId({postDocId, commentsDocId}) {
-  await firestore().collection('Post').doc(postDocId)
-  .collection('Comments').doc(commentsDocId).delete();
+async function deleteCommentsByDocId({ postDocId, commentsDocId }) {
+  await firestore()
+    .collection('Post')
+    .doc(postDocId)
+    .collection('Comments')
+    .doc(commentsDocId)
+    .delete();
 }
 
 // 내부로직 함수
-async function deleteImagesByDocId({postDocId, imagesDocId}) {
-  await deleteImage(imgName) // 스토리지 사진 지우는 것
-  await firestore().collection('Post').doc(postDocId) // 파이어베이스 DB 정보 지우는 것
-  .collection('Images').doc(imagesDocId).delete();
+async function deleteImagesByDocId({ postDocId, imagesDocId }) {
+  await deleteImage(imgName); // 스토리지 사진 지우는 것
+  await firestore()
+    .collection('Post')
+    .doc(postDocId) // 파이어베이스 DB 정보 지우는 것
+    .collection('Images')
+    .doc(imagesDocId)
+    .delete();
 }
 
-
-export {findPostById, findPostByTitle, findPostList, createPost, deletePost, updatePost,
-        postRangeUpdate, likeUpdate, createComments, deleteComments, updateComments, readComments, 
-        readImages, isMyPost, isMyComments}
+export {
+  findPostById,
+  findPostByTitle,
+  findPostList,
+  createPost,
+  deletePost,
+  updatePost,
+  postRangeUpdate,
+  likeUpdate,
+  createComments,
+  deleteComments,
+  updateComments,
+  readComments,
+  readImages,
+  isMyPost,
+  isMyComments,
+};
--- api/UserApi.js
+++ api/UserApi.js
@@ -3,85 +3,97 @@ import { getUserDocId } from './LogicApi';
 
 // 매개변수: 구글 auth, 유저ID
 async function createUser(userId, email) {
-    //유저ID를 기반으로 유저 생성
-    await firestore().collection('User').add({// auth와 합칠 때 유일성을 보장하는 값으로 구글이메일 쓸 예정!
-      id: userId,
-      email: email // auth로 저장된 이메일 넣기
-    });
-  }
+  //유저ID를 기반으로 유저 생성
+  await firestore().collection('User').add({
+    // auth와 합칠 때 유일성을 보장하는 값으로 구글이메일 쓸 예정!
+    id: userId,
+    email: email, // auth로 저장된 이메일 넣기
+  });
+}
 
 // 매개변수: 유저ID
-async function findUserById(userId) { // 유저ID로 유저 찾기
-    const result = [];
+async function findUserById(userId) {
+  // 유저ID로 유저 찾기
+  const result = [];
 
-    const user = await firestore().collection('User')
-    .where('id', '==', userId).get();
+  const user = await firestore().collection('User').where('id', '==', userId).get();
 
-    if(user.empty) { // 유저가 비어있을때 널값 리턴
-      console.log("해당하는 유저가 없습니다.");
-      return;
-    }
+  if (user.empty) {
+    // 유저가 비어있을때 널값 리턴
+    console.log('해당하는 유저가 없습니다.');
+    return;
+  }
 
-    user.forEach(doc => { // 콘솔 출력문
-      result.push(doc.data())
-    });
+  user.forEach((doc) => {
+    // 콘솔 출력문
+    result.push(doc.data());
+  });
 
-    return result;
+  return result;
 }
 
 // 매개변수: 나의ID, 상대방ID
-async function addFollowing({myId, yourId}) { //내 유저ID와 상대방의 유저ID를 받아 팔로우, 팔로잉 추가
-    const myFollowing = await getUserDocId(myId) // 내 문서ID
-    const yourFollower = await getUserDocId(yourId) // 상대방 문서ID
-  
-    await firestore().collection('User').doc(myFollowing).collection('following').add({
-      following: yourId
-    })
- 
-   await firestore().collection('User').doc(yourFollower).collection('follower').add({
-     follower: myId
-   })
+async function addFollowing({ myId, yourId }) {
+  //내 유저ID와 상대방의 유저ID를 받아 팔로우, 팔로잉 추가
+  const myFollowing = await getUserDocId(myId); // 내 문서ID
+  const yourFollower = await getUserDocId(yourId); // 상대방 문서ID
+
+  await firestore().collection('User').doc(myFollowing).collection('following').add({
+    following: yourId,
+  });
+
+  await firestore().collection('User').doc(yourFollower).collection('follower').add({
+    follower: myId,
+  });
 }
 
 // 매개변수: 유저ID
-async function findFollowingById(userId) { // 유저ID를 기반으로 팔로잉 찾기
+async function findFollowingById(userId) {
+  // 유저ID를 기반으로 팔로잉 찾기
   const result = [];
 
-  const userDocId = await getUserDocId(userId) // 유저 DocId가 필요하므로 호출
-  
-  const following = await firestore().collection('User')
-  .doc(userDocId).collection('following').get(); // 하위 컬렉션인 팔로잉 접근
-  
-  if(following.empty) { // 비어있을때 출력문
-    console.log("해당하는 유저가 없습니다.");
+  const userDocId = await getUserDocId(userId); // 유저 DocId가 필요하므로 호출
+
+  const following = await firestore()
+    .collection('User')
+    .doc(userDocId)
+    .collection('following')
+    .get(); // 하위 컬렉션인 팔로잉 접근
+
+  if (following.empty) {
+    // 비어있을때 출력문
+    console.log('해당하는 유저가 없습니다.');
     return;
   }
-  
-  following.forEach(doc => { // 배열에 넣기
-    result.push(doc.data())
+
+  following.forEach((doc) => {
+    // 배열에 넣기
+    result.push(doc.data());
   });
-  
+
   return result;
 }
-  
+
 // 매개변수: 유저ID
-async function findFollowerById(userId) { // 유저ID를 기반으로 팔로워 찾기
+async function findFollowerById(userId) {
+  // 유저ID를 기반으로 팔로워 찾기
   const result = [];
 
-  const userDocId = await getUserDocId(userId) // 유저 DocId가 필요하므로 호출
-  
-  const follower = await firestore().collection('User')
-  .doc(userDocId).collection('follower').get(); // 하위 컬렉션인 팔로워 접근
-  
-  if(follower.empty) { // 비어있을 때 출력문
-    console.log("해당하는 유저가 없습니다.");
+  const userDocId = await getUserDocId(userId); // 유저 DocId가 필요하므로 호출
+
+  const follower = await firestore().collection('User').doc(userDocId).collection('follower').get(); // 하위 컬렉션인 팔로워 접근
+
+  if (follower.empty) {
+    // 비어있을 때 출력문
+    console.log('해당하는 유저가 없습니다.');
     return;
   }
- 
-  follower.forEach(doc => { // 배열에 넣기
-    result.push(doc.data())
+
+  follower.forEach((doc) => {
+    // 배열에 넣기
+    result.push(doc.data());
   });
- 
+
   return result;
 }
 
@@ -90,60 +102,90 @@ async function findFollowerById(userId) { // 유저ID를 기반으로 팔로워 
 
 // 팔로워 팔로잉 집계함수
 // 매개변수: 유저ID
-async function countFollowing(userId) { // 팔로잉 몇 명 인지 찾는 함수
-  const followingCount = await findFollowingById(userId)
+async function countFollowing(userId) {
+  // 팔로잉 몇 명 인지 찾는 함수
+  const followingCount = await findFollowingById(userId);
 
-  console.log(followingCount.size)
+  console.log(followingCount.size);
 
-  return followingCount.size
-}
-  
-// 매개변수: 유저ID
-async function countFollower(userId) { // 팔로워가 몇 명인지 찾는 함수 
-  const followerCount = await findFollowerById(userId)
-  
-  console.log(followerCount.size)
-  
-  return followerCount.size 
-}
-
-
-// 매개변수: 나의 유저ID, 상대방 유저ID
-async function deleteFollowing({myId, yourId}) { // 내 팔로잉 삭제
-  const myFollowing = await getUserDocId(myId) // 내 문서ID
-  const yourFollower = await getUserDocId(yourId) // 상대방 문서ID
-  
-  await firestore().collection('User').doc(myFollowing) // 나의 팔로잉에서 상대방 삭제
-  .collection('following').doc(yourFollower).delete();
-  
-  await firestore().collection('User').doc(yourFollower) // 상대방의 팔로워에서 나 삭제
-  .collection('follower').doc(myFollowing).delete();
-}
-  
-// 매개변수: 나의 유저ID, 상대방 유저ID
-async function deleteFollower({myId, yourId}) { // 내 팔로워 삭제
-  const myFollower = await getUserDocId(myId) // 내 문서ID
-  const yourFollowing = await getUserDocId(yourId) // 상대방 문서ID
-  
-  await firestore().collection('User').doc(myFollower) // 나의 팔로워에서 상대방 삭제
-  .collection('follower').doc(yourFollowing).delete();
-  
-  await firestore().collection('User').doc(yourFollowing) // 상대방의 팔로잉에서 나 삭제
-  .collection('following').doc(myFollower).delete();
+  return followingCount.size;
 }
 
 // 매개변수: 유저ID
-async function duplicationId(userId) { // 아이디 중복을 검사하는 함수
+async function countFollower(userId) {
+  // 팔로워가 몇 명인지 찾는 함수
+  const followerCount = await findFollowerById(userId);
+
+  console.log(followerCount.size);
+
+  return followerCount.size;
+}
+
+// 매개변수: 나의 유저ID, 상대방 유저ID
+async function deleteFollowing({ myId, yourId }) {
+  // 내 팔로잉 삭제
+  const myFollowing = await getUserDocId(myId); // 내 문서ID
+  const yourFollower = await getUserDocId(yourId); // 상대방 문서ID
+
+  await firestore()
+    .collection('User')
+    .doc(myFollowing) // 나의 팔로잉에서 상대방 삭제
+    .collection('following')
+    .doc(yourFollower)
+    .delete();
+
+  await firestore()
+    .collection('User')
+    .doc(yourFollower) // 상대방의 팔로워에서 나 삭제
+    .collection('follower')
+    .doc(myFollowing)
+    .delete();
+}
+
+// 매개변수: 나의 유저ID, 상대방 유저ID
+async function deleteFollower({ myId, yourId }) {
+  // 내 팔로워 삭제
+  const myFollower = await getUserDocId(myId); // 내 문서ID
+  const yourFollowing = await getUserDocId(yourId); // 상대방 문서ID
+
+  await firestore()
+    .collection('User')
+    .doc(myFollower) // 나의 팔로워에서 상대방 삭제
+    .collection('follower')
+    .doc(yourFollowing)
+    .delete();
+
+  await firestore()
+    .collection('User')
+    .doc(yourFollowing) // 상대방의 팔로잉에서 나 삭제
+    .collection('following')
+    .doc(myFollower)
+    .delete();
+}
+
+// 매개변수: 유저ID
+async function duplicationId(userId) {
+  // 아이디 중복을 검사하는 함수
   const user = await findUserById(userId);
-  
-  const token = true 
-  
-  if(user.empty) {
-    token = false
-  } 
-  
+
+  const token = true;
+
+  if (user.empty) {
+    token = false;
+  }
+
   return token;
 }
 
-export { createUser, findUserById, addFollowing, findFollowingById, findFollowerById, countFollowing,
-        countFollower, deleteFollowing, deleteFollower, duplicationId }
+export {
+  createUser,
+  findUserById,
+  addFollowing,
+  findFollowingById,
+  findFollowerById,
+  countFollowing,
+  countFollower,
+  deleteFollowing,
+  deleteFollower,
+  duplicationId,
+};
