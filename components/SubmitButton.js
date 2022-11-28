026fd974edfe2d9b5f4acbc513dd7c740a2ba49f
Dong-hui-Kim <donghui11777@gmail.com>
2022-11-29 01:36:47 +0900

    [기타] 커밋잘못올림

--- components/SubmitButton.js
+++ components/SubmitButton.js
@@ -36,42 +36,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default SubmitButton;import React, { Component } from 'react';
-import { StyleSheet, TouchableOpacity, Text, View } from 'react-native';
-
-function SubmitButton(props) {
-  return (
-    <View style={[styles.container, props.style]}>
-      <TouchableOpacity onPress={() => props.navigation.navigate('Main')}>
-        <Text style={styles.submit}>Submit</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#3F51B5',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
-    borderRadius: 2,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.35,
-    shadowRadius: 5,
-    elevation: 2,
-    minWidth: 88,
-    paddingLeft: 16,
-    paddingRight: 16,
-  },
-  submit: {
-    color: '#fff',
-    fontSize: 14,
-  },
-});
-
 export default SubmitButton;
